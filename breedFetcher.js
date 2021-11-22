52f704dbce411e4b1185551c365ec5daf2786645
dikshantkunwar <dikshantkunwar@gmail.com>
2021-11-22 16:18:44 -0500

    fetch cat descriptions with breed name

---
+++ breedFetcher.js
@@ -0,0 +1,24 @@
+const request = require('request');
+
+const baseURL = "https://api.thecatapi.com/v1";
+const query = process.argv[2];
+
+const callbackfn = (data) => {
+  const returndata = JSON.parse(data);
+
+  if (returndata.length === 0) {
+    return `No cats with breed type ${query} found!`;
+  } else {
+    return returndata[0].description;
+  }
+};
+
+const responseJSON = request(baseURL + "/breeds/search?q=" + query, (err, res, body) =>{
+  if (!err) {
+    console.log(callbackfn(body));
+  } else {
+    console.log(`Request error : ${err}`);
+  }
+});
+
+
